93c1ee1d02ac9a38df71554faaa29c74631c3734
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-24 17:47:54 +0300

    add home css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Projects from './pages/Projects';
 import Courses from './pages/Courses';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import './stylesheets/home.css';
 
 function App() {
   return (
