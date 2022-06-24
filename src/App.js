8d72e25a19e5042a1b6fda4095f7c65527140eac
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-24 23:58:38 +0300

    menu bar style fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Courses from './pages/Courses';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import './stylesheets/home.css';
+import './stylesheets/header-footer.css';
 
 function App() {
   return (
