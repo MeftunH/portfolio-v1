4d6c06cb202881e4377a37479c457d18b107b03c
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-25 23:16:08 +0300

    make header responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,8 +13,8 @@ function Home() {
           <h1>MAFTUN HASHIMLI</h1>
 
           <div className="intro-content d-flex justify-content-between">
-            <p>Software Developer</p>
-            <button className="primary-button font-bold w-50">Get Started</button>
+            <p>Software Engineer</p>
+            <button className="primary-button font-bold">Get Started</button>
           </div>
         </div>
       </div>
