990c97cf97bfb3c9cdf3855459d09c1080f8f901
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-26 20:15:32 +0300

    implement career intro section

--- src/pages/Career.js
+++ src/pages/Career.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from "react";
+import Header from "../components/Header";
+import {IoCodeSlash} from "react-icons/io5";
 
 function Career() {
   return (
     <div>
-    <Header></Header>
-    <h1>Career</h1>
+      <Header />
+      <div className="container career-intro">
+        <div className="row flex-with-center mt-5">
+          <div className="col-md-6 n-box-interest-intro">
+            <div>
+              <h1 className="text-center">Career</h1>
+              <p>
+                Good ideas are not adopted automatically.They must be driven
+                into practice with courageous patience
+              </p>
+              <button className="primary-button">Get Started</button>
+            </div>
+          </div>
+          <div className="col-md-6 position-relative">
+            <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill="#0F62FE"
+                d="M50.5,-43.3C63.4,-24.1,70.3,-3.3,65.8,14.4C61.3,32.2,45.4,46.7,27.9,53.3C10.5,59.9,-8.5,58.5,-29.6,51.9C-50.7,45.4,-74,33.8,-80.6,15.4C-87.2,-3,-77.1,-28.2,-60.9,-48.1C-44.7,-68,-22.3,-82.6,-1.8,-81.2C18.8,-79.8,37.6,-62.4,50.5,-43.3Z"
+                transform="translate(100 100)"
+              />
+            </svg>
+
+            <IoCodeSlash 
+            color="white"
+            size='180'
+            className="position-absolute top-50 start-50 translate-middle"/>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Career
+export default Career;
