f527c90dab7f273595aec5fa407b3a08bc4ad7f3
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-27 02:59:20 +0300

    add jobs data

---
+++ src/pages/jobsData.js
@@ -0,0 +1,32 @@
+const jobsData = [
+{
+     title: 'Software Development Intern',
+     company: 'Telecom Invest',
+     date_range: 'Jan 2020 - Feb 2020',
+     technologies: 'C#,Php,Laravel',
+     about:'We were developing automation systems with core php'
+},
+{
+    title: 'Software Development Intern',
+    company: 'BITE CS',
+    date_range: 'Aug 2020 - Oct 2020',
+    technologies: 'Php,Laravel,Git,Vue,JS,Html,Css,Bootstrap',
+    about:'We were setting up the operations of the global cargo company and casual independent projects'
+},
+{
+    title: 'Software Developer',
+    company: 'BITE CS',
+    date_range: 'Oct 2020 - Jul 2021',
+    technologies: 'Php,Laravel,Git,Vue,JS,Html,Css,Bootstrap',
+    about:'We were setting up the operations of the global cargo company and casual independent projects'
+},
+{
+    title: 'Software Developer',
+    company: 'BITE CS',
+    date_range: 'Jul 2021 - Apr 2022',
+    technologies: 'Php,Laravel,Git,Vue,JS,Html,Css,Bootstrap,Laragon',
+    about:'We as agency,develop websites,mobile apps and systems for customers'
+},
+];
+
+export default jobsData;
