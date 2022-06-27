335fb49e527c6e4e8d86ac8877974e5be51bc3be
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-27 04:00:26 +0300

    update job data

--- src/pages/jobsData.js
+++ src/pages/jobsData.js
@@ -2,6 +2,8 @@ const jobsData = [
 {
      title: 'Software Development Intern',
      company: 'Telecom Invest',
+     logo: './assets/telecom-invest-logo.png',
+     location: 'Baku',
      date_range: 'Jan 2020 - Feb 2020',
      technologies: 'C#,Php,Laravel',
      about:'We were developing automation systems with core php'
@@ -9,6 +11,8 @@ const jobsData = [
 {
     title: 'Software Development Intern',
     company: 'BITE CS',
+    logo: './assets/bite-cs-logo.png',
+    location: 'Baku',
     date_range: 'Aug 2020 - Oct 2020',
     technologies: 'Php,Laravel,Git,Vue,JS,Html,Css,Bootstrap',
     about:'We were setting up the operations of the global cargo company and casual independent projects'
@@ -16,13 +20,17 @@ const jobsData = [
 {
     title: 'Software Developer',
     company: 'BITE CS',
+    logo: './assets/bite-cs-logo.png',
+    location: 'Baku',
     date_range: 'Oct 2020 - Jul 2021',
     technologies: 'Php,Laravel,Git,Vue,JS,Html,Css,Bootstrap',
     about:'We were setting up the operations of the global cargo company and casual independent projects'
 },
 {
     title: 'Software Developer',
-    company: 'BITE CS',
+    company: 'Epigra',
+    logo: './assets/epigra-logo.png',
+    location: 'Istanbul',
     date_range: 'Jul 2021 - Apr 2022',
     technologies: 'Php,Laravel,Git,Vue,JS,Html,Css,Bootstrap,Laragon',
     about:'We as agency,develop websites,mobile apps and systems for customers'
