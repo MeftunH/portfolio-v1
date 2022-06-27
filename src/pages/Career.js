52f77ccc0e96def30e2d21056027875df814c9e3
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-27 05:54:03 +0300

    change data folder structure

--- src/pages/Career.js
+++ src/pages/Career.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import {IoCodeSlash} from "react-icons/io5";
-import jobsData from './jobsData.js';
+import jobsData from '../data/jobsData';
 function Career() {
   return (
     <div>
